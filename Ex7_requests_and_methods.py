95d9a6e1cce9561f0befd0e1a3a5170a513982af
geogez <g.geogez@gmail.com>
2022-08-30 22:09:16 +0300

    Ex7_requests_and_methods_v_2

--- Ex7_requests_and_methods.py
+++ Ex7_requests_and_methods.py
@@ -21,31 +21,54 @@ response = requests.get(link, params={"method":"GET"})
 print(f"пункт дз 3, {response}")
 
 #4
-methods = ["get", "post", "put", "delete"]
-
+methods = ["get", "post", "put", "patch", "delete"]
 
 for i in methods:
     if i == "get":
         print("параметр get")
-        print(requests.get(link, params={"method":"GET"}).text)
-        print(requests.get(link, params={"method": "POST"}).text)
-        print(requests.get(link, params={"method": "PUT"}).text)
-        print(requests.get(link, params={"method": "DELETE"}).text)
+        print(requests.get(link, params={"method":"GET"}).text, "get")
+        print(requests.get(link, params={"method": "POST"}).text, "post")
+        print(requests.get(link, params={"method": "PUT"}).text, "put")
+        print(requests.get(link, params={"method": "DELETE"}).text, "delete")
+        print(requests.get(link, params={"method": "OPTIONS"}).text, "options")
+        print(requests.get(link, params={"method": "PATCH"}).text, "patch")
+        print(requests.get(link, params={"method": "HEAD"}).text, "head")
     elif i == "post":
         print("параметр post")
-        print(requests.post(link, data={"method": "GET"}).text)
-        print(requests.post(link, data={"method": "POST"}).text)
-        print(requests.post(link, data={"method": "PUT"}).text)
-        print(requests.post(link, data={"method": "DELETE"}).text)
+        print(requests.post(link, data={"method": "GET"}).text, "get")
+        print(requests.post(link, data={"method": "POST"}).text, "post")
+        print(requests.post(link, data={"method": "PUT"}).text, "put")
+        print(requests.post(link, data={"method": "DELETE"}).text, "delete")
+        print(requests.post(link, data={"method": "OPTIONS"}).text, "options")
+        print(requests.post(link, data={"method": "PATCH"}).text, "patch")
+        print(requests.post(link, data={"method": "HEAD"}).text, "head")
     elif i == "put":
         print("параметр put")
-        print(requests.put(link, data={"method": "GET"}).text)
-        print(requests.put(link, data={"method": "POST"}).text)
-        print(requests.put(link, data={"method": "PUT"}).text)
-        print(requests.put(link, data={"method": "DELETE"}).text)
+        print(requests.put(link, data={"method": "GET"}).text, "get")
+        print(requests.put(link, data={"method": "POST"}).text, "post")
+        print(requests.put(link, data={"method": "PUT"}).text, "put")
+        print(requests.put(link, data={"method": "DELETE"}).text, "delete")
+        print(requests.put(link, data={"method": "OPTIONS"}).text, "options")
+        print(requests.put(link, data={"method": "PATCH"}).text, "patch")
+        print(requests.put(link, data={"method": "HEAD"}).text, "head")
+
     elif i == "delete":
         print("параметр delete")
-        print(requests.delete(link, data={"method": "GET"}).text)
-        print(requests.delete(link, data={"method": "POST"}).text)
-        print(requests.delete(link, data={"method": "PUT"}).text)
-        print(requests.delete(link, data={"method": "DELETE"}).text)
+        print(requests.delete(link, data={"method": "GET"}).text, "get")
+        print(requests.delete(link, data={"method": "POST"}).text, "post")
+        print(requests.delete(link, data={"method": "PUT"}).text, "put")
+        print(requests.delete(link, data={"method": "DELETE"}).text, "delete")
+        print(requests.delete(link, data={"method": "OPTIONS"}).text, "options")
+        print(requests.delete(link, data={"method": "PATCH"}).text, "patch")
+        print(requests.delete(link, data={"method": "HEAD"}).text, "head")
+        #тип запроса delete не совпадает со значением параметра (get), но сервер отвечает так, словно все ок
+    elif i == "patch":
+        print("параметр patch")
+        print(requests.patch(link, data={"method": "GET"}).text, "get")
+        print(requests.patch(link, data={"method": "POST"}).text, "post")
+        print(requests.patch(link, data={"method": "PUT"}).text, "put")
+        print(requests.patch(link, data={"method": "DELETE"}).text, "delete")
+        print(requests.patch(link, data={"method": "OPTIONS"}).text, "options")
+        print(requests.patch(link, data={"method": "PATCH"}).text, "patch")
+        #тип запроса patch совпадает со значением параметра (patch), но сервер отвечает выдает ошибку
+        print(requests.patch(link, data={"method": "HEAD"}).text, "head")
