beba5001be11d941977d91334bdda58d7a6b74bd
geogez <g.geogez@gmail.com>
2022-09-01 21:43:13 +0300

    Ex11

--- Ex11.py
+++ Ex11.py
@@ -1,4 +1,18 @@
 import pytest
 import requests
+
+# link = "https://playground.learnqa.ru/api/homework_cookie"
+# response = requests.get(link).json()
+# print(response)
+
+
 link = "https://playground.learnqa.ru/api/homework_cookie"
-print(requests.get(link).text)
+response = requests.get(link)
+print(response.cookies.get('HomeWork'))
+cookies = response.cookies.get('HomeWork')
+
+
+def test_cookie():
+    assert cookies == "hw_value", f"Кука не равна значению 'hw_value'"
+
+
