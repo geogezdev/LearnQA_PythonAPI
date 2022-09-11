2ca1ee5da949481772b00b71cdee572747779c5d
geogez <g.geogez@gmail.com>
2022-09-11 21:11:04 +0300

    Sprint_IV

--- Ex14/tests/test_user_auth.py
+++ Ex14/tests/test_user_auth.py
@@ -3,58 +3,55 @@ import pytest
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
+
 # python - m pytest - s .\ex14\test_user_auth.py
 
 class TestUserAuth(BaseCase):
+    exclude_params = [
+        "no_cookie",
+        "no_token"
+    ]
 
-  exclude_params = [
-      ("no_cookie"),
-      ("no_token")
-  ]
+    def setup(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
 
-  def setup(self):
-    data = {
-        'email': 'vinkotov@example.com',
-        'password': '1234'
-    }
+        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
 
-    response1 = requests.post(
-        "https://playground.learnqa.ru/api/user/login", data=data)
+        self.auth_sid = self.get_cookie(response1, "auth_sid")
+        self.token = self.get_header(response1, "x-csrf-token")
+        self.user_id_from_auth_method = self.get_json_value(response1, "user_id")
 
-    self.auth_sid = self.get_cookie(response1, "auth_sid")
-    self.token = self.get_header(response1, "x-csrf-token")
-    self.user_id_from_auth_method = self.get_json_value(response1, "user_id")
+    def test_auth_user(self):
+        response2 = requests.get(
+            "https://playground.learnqa.ru/api/user/auth",
+            headers={"x-csrf-token": self.token},
+            cookies={"auth_sid": self.auth_sid}
+        )
 
-  def test_auth_user(self):
-    response2 = requests.get(
-      "https://playground.learnqa.ru/api/user/auth",
-      headers={"x-csrf-token": self.token},
-      cookies={"auth_sid": self.auth_sid}
-    )
-    
-    Assertions.assert_json_value_by_name(
-      response2, 
-      "user_id",
-      self.user_id_from_auth_method, 
-      "User id from auth method is not equel to user id from check method")
+        Assertions.assert_json_value_by_name(
+            response2,
+            "user_id",
+            self.user_id_from_auth_method,
+            "User id from auth method is not equel to user id from check method")
 
+    @pytest.mark.parametrize('condition', exclude_params)
+    def test_negative_auth_check(self, condition):
+        if condition == "no_cookie":
+            response2 = requests.get(
+                "https://playground.learnqa.ru/api/user/auth",
+                headers={"x-csrf-token": self.token}
+            )
+        else:
+            response2 = requests.get(
+                "https://playground.learnqa.ru/api/user/auth",
+                cookies={"auth_sid": self.auth_sid}
+            )
 
-  @pytest.mark.parametrize('condition', exclude_params)
-  def test_negative_auth_check(self, condition):
-    if condition == "no_cookie":
-      response2 = requests.get(
-        "https://playground.learnqa.ru/api/user/auth",
-        headers={"x-csrf-token": self.token}
-      )
-    else:
-      response2 = requests.get(
-        "https://playground.learnqa.ru/api/user/auth",
-        cookies={"auth_sid": self.auth_sid}
-      )
-
-    Assertions.assert_json_value_by_name(
-        response2,
-        "user_id",
-        0,
-        f"User is authorized with condition {condition}")
-
+        Assertions.assert_json_value_by_name(
+            response2,
+            "user_id",
+            0,
+            f"User is authorized with condition {condition}")
